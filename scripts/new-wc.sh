d38f22b7e03be76eae82cc682ee845828f91cc80
Jeyson Buitrago <jbuitrago@decodeit.co>
2023-02-27 15:48:21 +0100

    feat: included --organization param in new-wc command
    Included --organization @izwc parameter in the new-wc.sh
    
    Updated the testing wc hello-world
    
    Included support of .env file in the new-wc.sh

--- scripts/new-wc.sh
+++ scripts/new-wc.sh
@@ -1,6 +1,20 @@
+CURRENT_DIRECTORY=$(pwd)
+VARS_FILE="${CURRENT_DIRECTORY}/.new-wc-env.sh"
+ENV_FILE="${CURRENT_DIRECTORY}/../.env"
+
+# https://gist.github.com/mihow/9c7f559807069a03e302605691f85572?permalink_comment_id=3770333#gistcomment-3770333
+# Local .env
+# Importing variables from .env file using the ENV_FILE variable
+export $(grep -v '^#' $ENV_FILE | xargs)
+
 # Delete .new-wc-env.sh file if it exists
-if [ -f .new-wc-env.sh ]; then
-  rm .new-wc-env.sh
+if [ -f $VARS_FILE ]; then
+  rm $VARS_FILE
+fi
+
+# if ORGANIZATION is not set, then set it to the default value
+if [ -z "$ORGANIZATION" ]; then
+  ORGANIZATION="@izwc"
 fi
 
 # Run new-wc.js with nodejs (using prompts package)
@@ -14,8 +28,6 @@ if [ $exit_code -ne 0 ]; then
 fi
 
 # If the exit code is 0, then source the .new-wc-env.sh file
-CURRENT_DIRECTORY=$(pwd)
-VARS_FILE="${CURRENT_DIRECTORY}/.new-wc-env.sh"
 
 if [ -f $VARS_FILE ]; then
   . $VARS_FILE
@@ -25,7 +37,7 @@ else
 fi
 
 # Run the open-wc command with the arguments passed to this script
-npm init -w packages/$WC_ATOMIC_TYPE @izwc@latest -- --destinationPath $WC_PATH --type scaffold --scaffoldType wc --features linting testing demoing --typescript $WC_TYPESCRIPT --tagName $WC_TAG --writeToDisk true --installDependencies npm
+npm init -w packages/$WC_ATOMIC_TYPE @izwc@latest -- --organization $ORGANIZATION --destinationPath $WC_PATH --type scaffold --scaffoldType wc --features linting testing demoing --typescript $WC_TYPESCRIPT --tagName $WC_TAG --writeToDisk true --installDependencies npm
 
 # Delete the .new-wc-env.sh file
 rm -f $VARS_FILE
