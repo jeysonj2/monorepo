962d5c8149074c85fa6a5522b5b7e504b279495f
Jeyson Buitrago <jeysonj2@gmail.com>
2023-03-23 16:34:16 +0100

    refactor: implemented svg Interzero logo (from interzero.de)

--- packages/global/atoms/logo/src/style.css.ts
+++ packages/global/atoms/logo/src/style.css.ts
@@ -2,48 +2,25 @@ import { css } from 'lit';
 
 export const style = css`
   :host {
-    display: inline-grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 0.5fr;
-    grid-template-areas:
-      'inter zero'
-      'slog slog';
+    --logo-size-width: 176px;
+    --logo-size-height: 65px;
+    --logo-size-percentage: 1;
   }
+
   :host-context([size='small']) {
-    --typography-logo-font-size: 24px;
-    --typography-logo-line-height: 24px;
-    --typography-logob-font-size: 9.3px;
-    --typography-logob-letter-spacing: 0.135rem;
+    --logo-size-percentage: 0.6;
   }
 
-  iz-typography {
-    --typography-display: inline;
+  :host-context([size='medium']) {
+    --logo-size-percentage: 1;
   }
 
-  .inter {
-    grid-area: inter;
-    --typography-color: var(--logo-color-secondary, #06095a);
+  :host-context([size='large']) {
+    --logo-size-percentage: 1.4;
   }
-  .zero {
-    grid-area: zero;
-    --typography-color: var(--logo-color-primary, #5a61ff);
-  }
-  .slogan {
-    grid-area: slog;
-    --typography-color: var(--logo-color-secondary, #06095a);
+
+  .logo {
+    width: calc(var(--logo-size-width) * var(--logo-size-percentage));
+    height: calc(var(--logo-size-height) * var(--logo-size-percentage));
   }
 `;
-
-// h1.logo {
-//   font-size: var(--typography-logo-font-size, 36px);
-//   line-height: var(--typography-logo-line-height, 44px);
-//   font-weight: var(--default-typography-logo-font-weight, 600);
-//   font-family: var(--default-typography-logo-font-family, "Inter");
-// }
-// h4.logob {
-//   font-weight: var(--default-typography-logob-font-weight, 400);
-//   font-family: var(--typography-logob-font-family, "Inter");
-//   font-size: var(--typography-logob-font-size, 14px);
-//   line-height: var(--typography-logob-line-height, 17px);
-//   letter-spacing: var(--typography-logob-letter-spacing, 0.2rem);
-// }
